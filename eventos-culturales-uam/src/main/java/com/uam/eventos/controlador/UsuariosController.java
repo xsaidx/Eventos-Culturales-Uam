71a628a9a1b17a752487dd54b718468b7568bf29
Said <ildikoez@gmail.com>
2026-03-10 13:01:12 -0600

    Agregado controlador de Usuarios

---
+++ eventos-culturales-uam/src/main/java/com/uam/eventos/controlador/UsuariosController.java
@@ -0,0 +1,20 @@
+package com.uam.eventos.controlador;
+
+public class UsuariosController {
+
+    public void registrarUsuario() {
+        System.out.println("Usuario registrado");
+    }
+
+    public void listarUsuarios() {
+        System.out.println("Listado de usuarios");
+    }
+
+    public void actualizarUsuario() {
+        System.out.println("Usuario actualizado");
+    }
+
+    public void eliminarUsuario() {
+        System.out.println("Usuario eliminado");
+    }
+}
