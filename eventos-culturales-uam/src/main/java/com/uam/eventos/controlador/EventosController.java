77d99c21ffb7ac6feeab51601acce035424be591
Said <ildikoez@gmail.com>
2026-03-10 12:49:14 -0600

    Agregado proyecto Maven con controlador de Eventos

---
+++ eventos-culturales-uam/src/main/java/com/uam/eventos/controlador/EventosController.java
@@ -0,0 +1,20 @@
+package com.uam.eventos.controlador;
+
+public class EventosController {
+
+    public void crearEvento() {
+        System.out.println("Evento creado");
+    }
+
+    public void listarEventos() {
+        System.out.println("Listado de eventos");
+    }
+
+    public void actualizarEvento() {
+        System.out.println("Evento actualizado");
+    }
+
+    public void eliminarEvento() {
+        System.out.println("Evento eliminado");
+    }
+}
