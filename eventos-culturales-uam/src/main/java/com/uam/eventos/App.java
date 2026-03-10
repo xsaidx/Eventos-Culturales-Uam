954de15889651a60f257b3e85a8b42fe651c89a6
Said <ildikoez@gmail.com>
2026-03-10 12:30:36 -0600

    Proyecto Maven inicial

---
+++ eventos-culturales-uam/src/main/java/com/uam/eventos/App.java
@@ -0,0 +1,13 @@
+package com.uam.eventos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
